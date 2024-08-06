6c6e31e045edb1113338aa4e6dc083dce2de4fc2
oguzhan <oguzhan@ait.com.tr>
2024-08-06 12:23:28 +0300

    Refactored _credit_card.py

--- src/uix_components/_credit_card/_credit_card.py
+++ src/uix_components/_credit_card/_credit_card.py
@@ -1,42 +1,47 @@
 import os
-import io
-import uix
-from uix import T
-from uix.elements import row,button,div
+from io import open
+from uix import T, app, html, Element
+from uix.elements import row, button, div
 
-uix.html.add_css_file("_credit_card.css",__file__)
-uix.html.add_script_source(id="credit_card",script="imask.min.js",beforeMain=False,localpath=__file__)
-uix.html.add_script_source(id="credit_card_ui",script="_credit_card.js",beforeMain=False,localpath=__file__)
-uix.html.add_script_source(id="add_card_js",script="_add_card.js",beforeMain=False,localpath=__file__)
+base_path = os.path.dirname(__file__)
+public_path = os.path.join(base_path, "public")
 
+def add_resources(resource_type, resources):
+    for resource_id, filename in resources.items():
+        full_path = os.path.join(public_path, filename)
+        if resource_type == 'css':
+            html.add_css_file(filename, full_path)
+        elif resource_type == 'js':
+            html.add_script_source(id=resource_id, script=filename, beforeMain=False, localpath=full_path)
 
-path=os.path.join(os.path.dirname(os.path.dirname(__file__)), "_credit_card")
-uix.app.add_static_route("credit_card",path)
+add_resources('css', {'_credit_card': '_credit_card.css'})
+add_resources('js', {
+    'credit_card': 'imask.min.js',
+    'credit_card_ui': '_credit_card.js',
+    'add_card_js': '_add_card.js'
+})
 
-with io.open(os.path.join(path,"_credit_card.html"), mode="r", encoding="utf-8") as f:
-                html_content=f.read()
-                f.close
+app.add_static_route("credit_card", public_path)
 
-class credit_card(uix.Element):
-    def __init__(self, value, id = None, callback=None):
-            super().__init__(value,id = id)
-            self.path=path
-            self.callback=callback
-            self.size("100%","100%")
-            self.html_content=html_content
-            with self:
-                with row("",id="credit-cards").cls("credit-card-row") as creditcard:
-                    self.creditcard=creditcard
-                    with div(id="add_card_div").cls("field-container") as add_card_div:
-                        self.add_card_div=add_card_div
-                        button(T("Add Card"),id="payment-button",type="submit").cls("payment-button").on("add_card", self.callback)
-                
+with open(os.path.join(public_path, "_credit_card.html"), mode="r", encoding="utf-8") as f:
+    html_content = f.read()
+
+class credit_card(Element):
+    def __init__(self, value, id=None, callback=None):
+        super().__init__(value, id=id)
+        self.callback = callback
+        self.size("100%", "100%")
+        
+        with self:
+            with row("", id="credit-cards").cls("credit-card-row") as self.creditcard:
+                with div(id="add_card_div").cls("field-container") as self.add_card_div:
+                    button(T("Add Card"), id="payment-button", type="submit")\
+                        .cls("payment-button")\
+                        .on("add_card", self.callback)
+
     def init(self):
-        self.session.queue_for_send(self.id,{
+        self.session.queue_for_send(self.id, {
             'creditcard': self.creditcard.id,
-            'html_content': self.html_content,
+            'html_content': html_content,
             'add_card_div': self.add_card_div.id
-            }, 
-
-             "init-credit-card")
-
+        }, "init-credit-card")
